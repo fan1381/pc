73208bc2565c6f98927f28eda599b26f7119a498
范大侠 <3108484852@qq.com>
2020-03-14 16:12:29 +0800

    登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,23 +5,32 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt="" />
       </div>
+
       <!-- 表单 -->
-      <el-form class="form">
-        <el-form-item
-          ><el-input placeholder="请输入手机号"></el-input
+      <el-form class="form" :model="form" :rules="rules" ref="myForm">
+        <el-form-item prop="mobile"
+          ><el-input v-model="form.mobile" placeholder="请输入手机号"></el-input
         ></el-form-item>
 
-        <el-form-item
-          ><el-input style="width:65%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code"
+          ><el-input
+            v-model="form.code"
+            style="width:65%"
+            placeholder="请输入验证码"
+          ></el-input>
           <el-button type="primary" style="float:right" plain
             >发送验证码</el-button
           ></el-form-item
         >
 
-        <el-form-item><el-checkbox>您必须勾选协议 </el-checkbox></el-form-item>
+        <el-form-item prop="cheackBox"
+          ><el-checkbox v-model="form.cheackBox"
+            >您必须勾选协议
+          </el-checkbox></el-form-item
+        >
 
         <el-form-item
-          ><el-button type="primary" style="width:100%"
+          ><el-button @click="submit" type="primary" style="width:100%"
             >登录</el-button
           ></el-form-item
         >
@@ -31,18 +40,60 @@
 </template>
 
 <script>
+
 export default {
   name: '',
   props: {},
   components: {},
   data () {
-    return {}
+    return {
+      form: {
+        mobile: '',
+        code: '',
+        cheackBox: false
+      },
+      // 校验规则
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '手机号格式不正确'
+          }
+        ],
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码不正确' }
+        ],
+        cheackBox: [
+          {
+            validator: function (rule, value, callback) {
+              if (value) {
+                callback()
+              } else {
+                callback(new Error('你还没有勾选协议'))
+              }
+            }
+          }
+        ]
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    submit () {
+      this.$refs.myForm.validate((yes) => {
+        if (yes) {
+
+        } else {
+
+        }
+      })
+    }
+  }
 }
 </script>
 
