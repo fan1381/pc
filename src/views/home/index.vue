4b2c8230a5875eca81e2c9b49d557802491107d2
范大侠 <3108484852@qq.com>
2020-03-14 17:53:51 +0800

    home左侧

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,27 +1,46 @@
 <template>
-<div class='' >
-    主页
-</div>
+  <div class="">
+    <!-- 大容器 -->
+    <el-container>
+      <!-- 左侧 -->
+      <el-aside
+        style="min-height:100vh;background-color:#353b4e;width:230px"
+      >
+        <left></left>
+      </el-aside>
+      <!-- 右侧 -->
+      <el-container>
+        <el-header>
+          1
+        </el-header>
+
+        <el-main>
+          1
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import left from '@/components/home/aside'
 export default {
   name: '',
   props: {},
-  components: {},
+  components: {
+    left
+  },
   data () {
-    return {}
+    return {
+
+    }
   },
   computed: {},
   watch: {},
-  created () {
-  },
-  mounted () {
-  },
+  created () {},
+  mounted () {},
   methods: {}
 }
 </script>
 
-<style  scoped lang='less'>
-
-</style>
+<style scoped lang="less"></style>
