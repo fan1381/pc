7dc52f2a40113061e2ea6333947af4f0e5639561
范大侠 <3108484852@qq.com>
2020-03-16 19:51:58 +0800

    评论管理，页面表格结构，数据渲染

--- src/use/index.js
+++ src/use/index.js
@@ -1,12 +1,10 @@
 import left from '@/components/home/aside'
 import headers from '@/components/home/header'
 import crumb from '@/components/mianbao'
-// import routerView from '@/views/comment/index'
 export default {
   install (Vue) {
     Vue.component('left', left)
     Vue.component('headers', headers)
     Vue.component('crumb', crumb)
-    // Vue.component('router-view', routerView)
   }
 }
