1500c8b977f5a7de8ca52d8c3c56189eda52d25e
范大侠 <3108484852@qq.com>
2020-03-19 18:36:58 +0800

    文章列表，筛选功能，分页功能

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -9,7 +9,7 @@
       <!-- 搜索 -->
       <el-form style="padding-left:50px">
         <el-form-item label="文章状态：">
-          <el-radio-group v-model="radio">
+          <el-radio-group v-model="form.radio" @change="cahngeStatus">
             <el-radio :label="5">全部</el-radio>
             <el-radio :label="0">草稿</el-radio>
             <el-radio :label="1">待审核</el-radio>
@@ -19,14 +19,21 @@
         </el-form-item>
 
         <el-form-item label="频道列表：">
-          <el-select>
-            <el-option>sdsd</el-option>
+          <el-select v-model="form.id">
+            <el-option
+              v-for="item in channels"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
           </el-select>
         </el-form-item>
 
         <el-form-item label="时间选择：">
           <div class="block">
             <el-date-picker
+              value-format="yyyy-MM-dd"
+              v-model="form.date"
               type="daterange"
               range-separator="至"
               start-placeholder="开始日期"
@@ -37,7 +44,7 @@
         </el-form-item>
       </el-form>
 
-      <el-row class="text">共找到{{ this.list.length }}条符合条件的内容</el-row>
+      <el-row class="text">共找到{{ this.page.total}}条符合条件的内容</el-row>
 
       <!-- 列表 -->
       <div class="box" v-for="item in list" :key="item.id.toString()">
@@ -61,20 +68,44 @@
           <span><i class="el-icon-delete"></i>删除</span>
         </div>
       </div>
+
+      <!-- 分页 -->
+      <el-row type="flex" justify="center" style='margin-top:15px;'>
+        <el-pagination
+          :page-size="page.pageSize"
+          :current-page="page.currentPage"
+          layout="prev, pager, next"
+          :total="page.total"
+          @current-change='changePage'
+        >
+        </el-pagination>
+      </el-row>
     </el-card>
   </div>
 </template>
 
 <script>
+// import func from '../../../vue-temp/vue-editor-bridge'
 export default {
   name: '',
   props: {},
   components: {},
   data () {
     return {
-      radio: 0,
+      form: {
+        radio: 5,
+        date: [],
+        id: null
+      },
+      page: {
+        total: 0,
+        pageSize: 10,
+        currentPage: 1
+
+      },
       img: require('../../assets/img/404.png'),
-      list: [] // 文章列表
+      list: [], // 文章列表
+      channels: [] // 频道列表
     }
   },
   filters: {
@@ -94,20 +125,54 @@ export default {
     }
   },
   computed: {},
-  watch: {},
+  watch: {
+    form: {
+      handler: function () {
+        this.cahngeStatus()
+      },
+      deep: true
+    }
+  },
   created () {
     this.getArticle()
+    this.getChannel()
   },
   mounted () {},
   methods: {
-    getArticle () {
+    cahngeStatus () {
+      const params = {
+        status: this.form.radio === 5 ? null : this.form.radio,
+        channel_id: this.form.id,
+        begin_pubdate: this.form.date.length ? this.form.date[0] : null,
+        end_pubdate: this.form.date.length > 1 ? this.form.date[1] : null,
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
+      this.getArticle(params)
+    },
+    getArticle (params) {
       this.$axios({
-        url: '/articles'
+        url: '/articles',
+        params
       }).then(res => {
         this.list = res.data.data.results
-        console.log(this.list)
+        this.page.total = res.data.data.total_count
+        console.log(res)
       })
+    },
+    // 获取文章频道
+    getChannel () {
+      this.$axios({
+        url: 'channels'
+      }).then(res => {
+        this.channels = res.data.data.channels
+      })
+    },
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.cahngeStatus()
     }
+
   }
 }
 </script>
