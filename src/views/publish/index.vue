cfa75a70f3530eab24b91ce66a33268a798d53ad
范大侠 <3108484852@qq.com>
2020-03-20 16:31:37 +0800

    发布文章，简单发布文章，校验

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,40 +1,54 @@
 <template>
   <div class="">
-    <el-card>
-      <!-- 面包 -->
+    <el-card v-loading="loading"
+      ><!-- 面包 -->
       <crumb slot="header">
         <div slot="title">
           发布文章
         </div>
       </crumb>
       <!-- 内容 -->
-      <el-form style="margin-left:50px" label-width="100px">
-        <el-form-item label="标题">
+      <el-form
+        ref="myForm"
+        :model="form"
+        :rules="rules"
+        style="margin-left:50px"
+        label-width="100px"
+      >
+        <el-form-item label="标题" prop="title">
           <el-input v-model="form.title" style="width:60%"></el-input>
         </el-form-item>
 
-        <el-form-item label="内容">
-          <quill-editor v-model="form.cotent" style="height:300px;margin-bottom:100px" ></quill-editor>
+        <el-form-item label="内容" prop="content">
+          <quill-editor
+            v-model="form.content"
+            style="height:300px;margin-bottom:100px"
+          ></quill-editor>
         </el-form-item>
 
-        <el-form-item label="封面">
-            <el-radio-group v-model="form.radio">
-                 <el-radio :label="1">单图</el-radio>
-                  <el-radio :label="3">三图</el-radio>
-                  <el-radio :label="0">无图</el-radio>
-                  <el-radio :label="-1">自动</el-radio>
-            </el-radio-group>
+        <el-form-item label="封面" prop="cover">
+          <el-radio-group v-model="form.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
+          </el-radio-group>
         </el-form-item>
 
-        <el-form-item label="频道">
-            <el-select v-model="channels.id">
-                <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
-            </el-select>
+        <el-form-item label="频道" prop="channel_id">
+          <el-select v-model="form.channel_id">
+            <el-option
+              v-for="item in channels"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            ></el-option>
+          </el-select>
         </el-form-item>
 
         <el-form-item>
-            <el-button type="primary">发布</el-button>
-            <el-button>存草稿</el-button>
+          <el-button type="primary" @click="send">发布</el-button>
+          <el-button>存草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -42,6 +56,7 @@
 </template>
 
 <script>
+// import { Loading } from 'element-ui'
 export default {
   name: '',
   props: {},
@@ -50,10 +65,20 @@ export default {
     return {
       form: {
         title: '',
-        cotent: '',
-        radio: 0
+        content: '',
+        cover: {
+          type: 0,
+          images: []
+        },
+        channel_id: null
       },
-      channels: []
+      channels: [],
+      rules: {
+        title: [{ required: true, message: '不能为空' }],
+        content: [{ required: true, message: '文章内容不能为空' }],
+        channel_id: [{ required: true, message: '文章频道不能为空' }]
+      },
+      loading: false
     }
   },
   computed: {},
@@ -63,11 +88,32 @@ export default {
   },
   mounted () {},
   methods: {
+
     getChannels () {
       this.$axios({
         url: '/channels'
-      }).then((res) => {
+      }).then(res => {
         this.channels = res.data.data.channels
+      })
+    },
+    send () {
+      this.$refs.myForm.validate((yes) => {
+        if (yes) {
+          this.loading = true
+          this.$axios({
+            url: '/articles',
+            method: 'post',
+            params: { draft: false },
+            data: this.form
+          }).then((res) => {
+            this.loading = false
+            this.$message('发布成功')
+            // this.$router.push('/home/articles')
+          }).catch(() => {
+            this.loading = false
+            this.$message('发布失败')
+          })
+        }
       })
     }
   }
